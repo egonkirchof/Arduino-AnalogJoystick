fecec2ea0da94459b0474f7f029827583fec6e7a
egonkirchof <egonkirchof@yahoo.com.br>
2018-08-09 23:41:58 +0200

    Add files via upload

---
+++ Analog_Joystick2.ino
@@ -0,0 +1,42 @@
+// this is a modified program from a Elegoo kit to read an analog joystick
+// since my joystick would not give the same reading even when I hold it still
+// I add some delta to x and y so I consider two reading to be the same 
+// If they are within the delta
+
+// Arduino pin numbers
+const int SW_pin = 2; // digital pin connected to switch output
+const int X_pin = 0; // analog pin connected to X output
+const int Y_pin = 1; // analog pin connected to Y output
+
+struct data {
+  int sw;
+  int x;
+  int y;  
+} previous,now;
+
+#define Dy 1 // if the joystick keeps changing value even if you are holding it still
+#define Dx 1 // you can adjust these deltas and treat a small difference compared to the
+             // previous reading as the same reading
+             
+
+void setup() {
+  pinMode(SW_pin, INPUT_PULLUP);
+  Serial.begin(9600);
+  previous.x = 313;
+}
+
+bool different(data a,data b) {
+  return !(a.sw==b.sw and ( abs(a.x-b.x)<=Dx) and ( abs(a.y-b.y)<=Dy));
+}
+
+void loop() {
+  now.sw = digitalRead(SW_pin);
+  now.x = analogRead(X_pin);
+  now.y = analogRead(Y_pin);
+  if( different(previous,now)) {
+    Serial.println(String(now.sw)+": "+ String(now.x) + " , "+ String(now.y));
+  }
+  previous = now;
+  delay(5); // not really necessary
+  
+}
